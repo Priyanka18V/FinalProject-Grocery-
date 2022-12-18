bb8952f5cf701c822f539ed7d482e0650944147d
Priyanka18V <112681687+Priyanka18V@users.noreply.github.com>
2022-12-18 13:37:10 +0530

    Delete GroceryShopping directory

--- GroceryShopping/src/main/java/com/example/demo/domain/Status.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.domain;
-
-public enum Status {
-    
-    Pending, Shipped, Delivered, Acquired;
-}
--- GroceryShopping/src/main/java/com/example/demo/validation/UserValidationService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.validation;
-
-import com.example.demo.domain.model.UserServiceModel;
-
-public interface UserValidationService {
-    boolean isValid(UserServiceModel user);
-}
